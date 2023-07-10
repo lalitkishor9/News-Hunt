fd9ad67eaaab16cca8070e558699e702e378c14c
Lalit Kishor <lalitkishor202125@gmail.com>
2023-07-10 11:48:55 +0530

    updated

--- src/component/NewsItem.js
+++ src/component/NewsItem.js
@@ -9,7 +9,7 @@ export class NewsItem extends Component {
         <div className="card" >
         <span className="position-absolute top-0  translate-middle  bg-success text-light  rounded " style={{left:"85%" , zIndex:1, fontSize:"12px"}}>{source}
           </span>
-        <img src={imageUrl?imageUrl:"https://images.hindustantimes.com/tech/img/2023/06/28/1600x900/bgmi_1687954108648_1687954113403.jpg"} className="card-img-top" alt="..."/>
+        <img src={imageUrl?imageUrl:"no_image.png"} className="card-img-top" alt="..."/>
         <div className="card-body">
           <h5 className="card-title">{title}</h5>
           
